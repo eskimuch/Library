411ae4b90f3d9ded3515844f243812fbba0d43f6
eskimuch <eskimuch@gmail.com>
2017-12-16 21:14:31 +0100

    v0.6 DataReader class and scanner added

---
+++ src/utils/DataReader.java
@@ -0,0 +1,37 @@
+package utils;
+import app.Book;
+import java.util.Scanner;
+
+public class DataReader {
+
+    private Scanner sc;
+
+    public DataReader()
+    {
+        sc = new Scanner(System.in);
+    }
+    public void close()
+    {
+        sc.close();
+    }
+
+    public Book readAndCreateBook()
+    {
+        System.out.println("\nTytuł: ");
+        String title = sc.nextLine();
+        System.out.println("Autor: ");
+        String author = sc.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int releaseDate = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ilość stron: ");
+        int pages = sc.nextInt();
+        sc.nextLine();
+
+        return new Book(title,author,releaseDate,pages,publisher,isbn);
+    }
+}
